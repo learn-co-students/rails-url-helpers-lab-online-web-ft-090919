6826c413446498da5bf7413bce5272f423116330
Kenneth Spatta <kenyounot@gmail.com>
2019-11-05 20:55:05 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Kenneth", last_name: "Spatta")
+Student.create(first_name: "Kennedy", last_name: "Spatta")
